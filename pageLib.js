98108647736819342e41aeb8f4c88a72001f0006
chicken <kevinisnowonline@gmail.com>
2015-02-20 17:18:00 +0800

    Complete Exercise # 1 - Making Images larger when clicked.

---
+++ pageLib.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+   
+    var isLarge = false;
+
+    $('img').click(function () {
+
+        
+
+        if (!isLarge) {
+            $(this).css({
+                'width': '600px',
+                'height': '180px'
+            })
+        } else {
+            $(this).css({
+                'width': '300px',
+                'height': '90px'
+            })
+        }
+        
+        isLarge = !isLarge;
+
+
+    })
+
+});
