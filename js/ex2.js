7fdcfb542a67e7143c24629b68e2709586d4b541
chicken <kevinisnowonline@gmail.com>
2015-02-21 00:01:13 +0800

    Successfully finished Exercise #2. re-structured the solution folder for maintainability.

---
+++ js/ex2.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    
+    // Set target string to be replaced, this time it is "XYZ"
+    var targetString = "XYZ";
+    var replacementString = "ABC";
+
+    $('input[type="button"]').click(function () {
+
+     
+        // Stylize the actual role for readability
+        $("p:contains('"+targetString+"')").text(function () {
+           var retVal =  $(this).text().replace(targetString, replacementString);
+
+            var temp = replacementString;
+            replacementString = targetString;
+            targetString = temp;
+
+            return retVal;
+        });
+
+
+
+     
+    })
+});
